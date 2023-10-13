f57737fbd0a17ade57e957b2f34cbf5d9b3728b2
TomOwen93 <136441914+TomOwen93@users.noreply.github.com>
2023-10-13 16:52:58 +0100

    Initialize example-cluster

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import { Scene } from 'three';
+import { setupCamera } from './setupCamera';
+import { setupHelpers } from './setupHelpers';
+import { setupLights } from './setupLights';
+import { setupOrbitControls } from './setupOrbitControls';
+import { setupRenderer } from './setupRenderer';
+import { setupShapeCluster } from './setupShapeCluster'
+
+//Try out this alternative
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { setupSymmetricalShapeCluster } from './setupSymmetricalShapeCluster';
+
+export function setupThreeJSScene(): void {
+
+    const dimensions = { w: window.innerWidth, h: window.innerHeight };
+
+    const camera = setupCamera(dimensions);
+
+    const renderer = setupRenderer(camera, dimensions);
+
+    const controls = setupOrbitControls(camera, renderer.domElement);
+
+    const scene = new Scene();
+
+    setupLights(scene);
+
+    setupHelpers(scene);
+
+    setupShapeCluster(scene);
+    //setupSymmetricalShapeCluster(scene);
+
+    setupKeyHandlers();
+
+    animate();
+
+
+
+    function animate() {
+
+        renderer.render(scene, camera);
+
+        // required if controls.enableDamping or controls.autoRotate are set to true
+        controls.update();
+
+        requestAnimationFrame(animate);
+    }
+
+    function setupKeyHandlers(): void {
+
+        function handleKeyDown(event: KeyboardEvent) {
+            if (event.key === "r") {
+                controls.autoRotate = !controls.autoRotate;
+            }
+        }
+        document.addEventListener("keydown", handleKeyDown);
+    }
+}
+
+setupThreeJSScene();
---
+++ src/setupShapeCluster.ts
@@ -0,0 +1,34 @@
+import { Scene, BoxGeometry, MeshStandardMaterial, Color, Mesh } from "three";
+import { randFloat, randFloatSpread } from "three/src/math/MathUtils";
+
+import { pick } from "./randomUtils";
+
+export function setupShapeCluster(scene: Scene): void {
+    //From https://nice-colours-quicker.netlify.app/
+    const palette = [
+        "#00a0b0",
+        "#6a4a3c",
+        "#cc333f",
+        "#eb6841",
+        "#edc951"
+    ];
+
+    for (let i = 0; i < 40; i++) {
+        const w = randFloat(8, 16);
+        const h = randFloat(8, 16);
+        const d = randFloat(8, 16);
+
+        const geometry = new BoxGeometry(w, h, d);
+        const material = new MeshStandardMaterial({
+            color: new Color(pick(palette))
+        });
+
+        const myShape: Mesh = new Mesh(geometry, material);
+        const x = randFloatSpread(40);
+        const y = randFloatSpread(40);
+        const z = randFloatSpread(40);
+        myShape.position.set(x, y, z);
+
+        scene.add(myShape);
+    }
+}
